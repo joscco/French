ab85dc178ea1d6266c131258af6e4360cee5c681
Jonathan Schmitz <jomitz96@hotmail.com>
2025-11-15 21:49:08 +0100

    feat: add tooltips to icon buttons for improved accessibility and user experience

--- src/app/icon-button/icon-button.component.ts
+++ src/app/icon-button/icon-button.component.ts
@@ -1,6 +1,7 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, ElementRef, EventEmitter, HostListener, Input, Output, ViewChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {MatIcon} from '@angular/material/icon';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-icon-button',
@@ -11,4 +12,54 @@ import {MatIcon} from '@angular/material/icon';
 export class IconButtonComponent {
   @Input() iconName: string = '';
   @Output() onClick = new EventEmitter<void>();
+
+  @Input() tooltip?: string = '';
+
+  tooltipYHidden = -35;
+  tooltipYVisible = -40;
+
+  @ViewChild('tooltipEl') tooltipEl?: ElementRef<HTMLSpanElement>;
+
+  @HostListener('mouseenter') onMouseEnter() {
+    queueMicrotask(() => this.animateTooltipIn());
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.animateTooltipOut();
+  }
+
+  @HostListener('focus') onFocus() {
+    queueMicrotask(() => this.animateTooltipIn());
+  }
+
+  @HostListener('blur') onBlur() {
+    this.animateTooltipOut();
+  }
+
+  private animateTooltipIn() {
+    if (!this.tooltipEl || !this.tooltip) {
+      return;
+    }
+    const el = this.tooltipEl.nativeElement;
+    gsap.killTweensOf(el);
+    gsap.fromTo(
+      el,
+      {opacity: 0, y: this.tooltipYHidden},
+      {opacity: 1, y: this.tooltipYVisible, duration: 0.18, ease: 'power2.out'}
+    );
+  }
+
+  private animateTooltipOut() {
+    if (!this.tooltipEl || !this.tooltip) {
+      return;
+    }
+    const el = this.tooltipEl.nativeElement;
+    gsap.killTweensOf(el);
+    gsap.to(el, {
+      opacity: 0,
+      y: this.tooltipYHidden,
+      duration: 0.15,
+      ease: 'power2.in'
+    });
+  }
 }
